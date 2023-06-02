b0f16ab6bceee7f94559776e1f67951bdb526d2f
Stanislav Kintoriak <kintoriak@gmail.com>
2023-06-02 22:30:53 +0300

    Commit 2
    1) added basename to BrowserRoater;

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-     <BrowserRouter>
+     <BrowserRouter basename="/your_repo_name">
       <App />
     </BrowserRouter>
   </React.StrictMode> 
